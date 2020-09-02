13865139d94df581175f51747c8581e8fa26f267
NabeelZanabeet <nabeelzanabeet@gmail.com>
2020-09-02 23:13:31 +0300

    Created users module & resolver with simple hello string query

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
